8f470508d3d6695f11be79bc1ae190564375404f
javiersrojas <javiersrojas@152.23.34.139>
2019-09-01 22:31:57 -0400

    pushing to github

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -12,53 +12,54 @@ public class A1Novice {
 		
 		int count = scan.nextInt();
 		
-		// Reads first name of the customer
+		for (int j=0; j<count; j++) {
 		
-		String firstName = scan.next();
+			// Reads first name of the customer
+			
+			String firstName = scan.next();
+			
+			// Reads last names of customer
+			
+			String lastName = scan.next();
+			
+			// Reads number of items bought by customer
+			
+			int items = scan.nextInt();
+			
+			// Create an array to store numbers
+			
+			int[] quantities = new int[items];
+			
+			// Create an array to store names of purchased items
+			
+			String[] itemName = new String[items];
+			
+			// Create an array to store price values
+			
+			double[] prices = new double[items];
+			
+			// Initializes variable to hold total cost
+			
+			double total = 0;
+			
+			// Read values into arrays
 		
-		// Reads last names of customer
-		
-		String lastName = scan.next();
-		
-		// Reads number of items bought by customer
-		
-		int items = scan.nextInt();
-		
-		// Create an array to store numbers
-		
-		int[] quantities = new int[count];
-		
-		// Create an array to store names of purchased items
-		
-		String[] itemName = new String[count];
-		
-		// Create an array to store price values
-		
-		double[] prices = new double[count];
-		
-		// Initializes variable to hold total cost
-		
-		double total = 0;
-		
-		// Read values into arrays
-		
-		for (int i=0; i<items; i++) {
-			quantities[i] = scan.nextInt();
-			itemName[i] = scan.next();
-			prices[i] = scan.nextDouble();
-			total += (quantities[i] * prices[i]);
-	}
-		
+			for (int i=0; i<items; i++) {
+				quantities[i] = scan.nextInt();
+				itemName[i] = scan.next();
+				prices[i] = scan.nextDouble();
+				total += (quantities[i] * prices[i]);
+			}	
+			// Convert total value to a string
+			
+			String totalString = String.format("%.2f", total);
+
+			// Print results
+			
+			System.out.println(firstName.charAt(0) + ". " + lastName + ": " + totalString);
+}
 		// Close Scanner
 		
 		scan.close();
-		
-		// Convert total value to a string
-		
-		String totalString = String.format("%.2f", total);
-
-		// Print results
-		
-		System.out.println(firstName.charAt(0) + ". " + lastName + ": " + totalString);
 	}
 }
